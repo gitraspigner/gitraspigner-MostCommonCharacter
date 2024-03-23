a62baf0acc54d8f6f1dc561b719c106d1b1bfd30
Ravi Spigner <585pieces@protonmail.com>
2024-03-23 22:33:52 +0000

    completed Coding Challenge: Most Common Character

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +9,26 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        char[] chars = str.toCharArray();
+        HashMap<Character, Integer> results = new HashMap<Character, Integer>();
+        
+        for (char c : chars) {
+            if (results.containsKey(c)) {   // if already entry, increment entry
+                results.put(c, results.get(c)+1);
+            } else { // if no entry, create one
+                results.put(c, 1);
+            }
+        }
+
+        int maxOccurence = 1;
+        char mostCommonChar = str.charAt(0);
+        for (char c : results.keySet()) {
+            if (results.get(c) > maxOccurence) {
+                maxOccurence = results.get(c);
+                mostCommonChar = c;
+            }
+        }
+        return mostCommonChar;
+       
     }
 }
